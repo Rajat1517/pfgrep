db2acc0418bb62558750ec3f6a1cc6248e1e7e5b
Rajat Mishra <rajatmishra08152gmail.com>
2025-11-05 08:52:23 +0000

    Added a safe queue for threads synchronization

---
+++ src/SafeQueue.cpp
@@ -0,0 +1,66 @@
+#include <pthread.h>
+#include <queue>
+#include <string>
+
+
+
+class SafeQueue{
+
+    std::queue<std::string> q;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond_pop;
+    bool done= false; 
+
+    public:
+
+    SafeQueue(){
+        pthread_mutex_init(&mutex, nullptr);
+        pthread_cond_init(&cond_pop, nullptr);
+    }
+
+    void push(const std::string &path){
+
+        pthread_mutex_lock(&mutex);
+
+        q.push(path);
+
+        pthread_cond_signal(&cond_pop);
+
+        pthread_mutex_unlock(&mutex);
+    }
+
+    std::string pop(){
+
+        pthread_mutex_lock(&mutex);
+
+        while(q.empty() && !done){
+            pthread_cond_wait(&cond_pop, &mutex);
+        }
+
+
+        if(q.empty() && done){
+            pthread_mutex_unlock(&mutex);
+            return "";
+        }
+        
+
+        std::string res= q.front();
+        q.pop();
+
+        pthread_mutex_unlock(&mutex);
+
+        return res;
+    }
+
+    void setDone(){
+        pthread_mutex_lock(&mutex);
+        done= true;
+        pthread_cond_broadcast(&cond_pop);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    ~SafeQueue(){
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&cond_pop);
+    }
+};
